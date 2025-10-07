5650bcc570d663542e97e5875559dbc7b57f4425
Julianto <jjulianto08@gmail.com>
2025-10-07 12:39:42 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'adhivasindo-test',
+  webDir: 'dist'
+};
+
+export default config;
